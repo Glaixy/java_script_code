48ca70b787b84a1ad16daa2c914e04e8ea1af1c5
Glaixy <glaixy2007@gmail.com>
2026-05-12 23:08:50 +0530

    added 2nd cod

---
+++ 2nd.js
@@ -0,0 +1,11 @@
+//WAP that only print multiple of no. between num 1 and num 2
+let num1=1
+let num2=10
+let multiple= 5
+
+for(let i=num1; i<=num2; i++){
+    if (i%multiple==0){
+        console.log(i)
+    }
+    
+}
---
+++ 3rd.js
@@ -0,0 +1,8 @@
+//WAP that print only odd no. between num 1 and num 2
+const num1=1
+const num2=10
+for(let i=num1; i<num2; i++){
+    if(i%2!=0){
+        console.log(i)
+    }
+}
