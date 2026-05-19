66db3b362f44e5cfa5c6339fe33a4bfc56244188
Glaixy <glaixy2007@gmail.com>
2026-05-19 19:46:39 +0530

    Add files via upload

---
+++ 10th.js
@@ -0,0 +1,5 @@
+//fizz buzz : print no. from 1 to 100
+//if no. is divisible by 5 then print "fizz"
+//if no. is divisible by 3 then print "buzz"
+//if no. is divisible by 3 and 5 then print "fizzbuzz"
+//if no. is not divisible by 3 and 5 then print the no
---
+++ array.js
@@ -0,0 +1,2 @@
+const user=[2,4,1,7,8]
+console.log(user.length)
