545451646b923889b2faefe41ebef5ac33308c2d
Glaixy <glaixy2007@gmail.com>
2026-05-19 19:45:26 +0530

    Add files via upload

---
+++ 6th.js
@@ -0,0 +1,9 @@
+//1.Even or odd : write a function to check if a number is even or odd
+function EvenOrOdd(num){
+    if(num%2==0){
+        console.log("Even")
+    }else{
+        console.log("odd")
+    }
+}
+EvenOrOdd(4)
---
+++ 7th.js
@@ -0,0 +1,10 @@
+//2. sum of no. using loop eg. sumofnum(5) =1+2+3+4+5=15
+function sumofnum(num){
+    let sum=0
+    for(let i=1; i<=num; i++){
+        sum=sum+i
+
+    }
+    console.log(sum)
+}
+sumofnum(5)
